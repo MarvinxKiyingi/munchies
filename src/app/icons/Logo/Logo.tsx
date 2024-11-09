21af2290d3289c939a07b72f72db65fefe4a2484
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2024-11-09 17:27:22 +0100

    feat(Logo): turn logo into a link

--- src/app/icons/Logo/Logo.tsx
+++ src/app/icons/Logo/Logo.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import MobileLogo from './MobileLogo';
 import DesktopLogo from './DesktopLogo';
+import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <>
+    <Link href={'/'}>
       <MobileLogo />
       <DesktopLogo />
-    </>
+    </Link>
   );
 };
 
