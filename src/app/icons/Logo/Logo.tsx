85065236e00c1455c3970a857c9a8c44f6218fc9
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2024-11-09 14:55:54 +0100

    fix(Logo): add device-specific logos

---
+++ src/app/icons/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MobileLogo from './MobileLogo';
+import DesktopLogo from './DesktopLogo';
+
+const Logo = () => {
+  return (
+    <>
+      <MobileLogo />
+      <DesktopLogo />
+    </>
+  );
+};
+
+export default Logo;
